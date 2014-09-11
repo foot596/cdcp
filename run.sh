4e24f51798fee8776ccc343b69cd91a46685e23f
Dmitry Zadvornykh <dmitry@zadvornykh.com>
2014-09-11 10:58:52 -0500

    Fix

--- run.sh
+++ run.sh
@@ -7,8 +7,12 @@ LOG_DIR="log"
 LOG_FILE_CORE="$LOG_DIR/cdcp_core.log"
 
 OLD_HEAD=$(git rev-parse HEAD)
+echo "[$(date +"%d-%m-%Y %H:%M:%S")] Current revision: $OLD_HEAD" | tee -a $LOG_FILE_CORE
+
 git pull origin master | tee -a $LOG_FILE_CORE
 NEW_HEAD=$(git rev-parse HEAD)
+echo "[$(date +"%d-%m-%Y %H:%M:%S")] New revision: $NEW_HEAD" | tee -a $LOG_FILE_CORE
+
 if [ $OLD_HEAD != $NEW_HEAD ]; then
   echo "[$(date +"%d-%m-%Y %H:%M:%S")] Script updated. Restarting" | tee -a $LOG_FILE_CORE
   exec ./run.sh
